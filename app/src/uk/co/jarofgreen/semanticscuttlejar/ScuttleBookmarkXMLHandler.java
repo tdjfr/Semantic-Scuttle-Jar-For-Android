e7173c19ac2f1ea5889c4f51122095db15afa2ad
David <github@cure.nom.fr>
2013-08-16 22:35:39 +0200

    Display all url status
       P : private
       S : shared
       otherwise : public

--- app/src/uk/co/jarofgreen/semanticscuttlejar/ScuttleBookmarkXMLHandler.java
+++ app/src/uk/co/jarofgreen/semanticscuttlejar/ScuttleBookmarkXMLHandler.java
@@ -20,15 +20,23 @@ public class ScuttleBookmarkXMLHandler extends DefaultHandler {
 		if( localName.equalsIgnoreCase("post") ) {
 			HashMap<String, String> curBookmark = new HashMap<String, String>();
 			curBookmark.put("href", attributes.getValue("href"));
-            if (Integer.valueOf(attributes.getValue("status")) == 2)
-                curBookmark.put("description", "P / "+attributes.getValue("description"));
-            else
-			    curBookmark.put("description", attributes.getValue("description"));
+            		switch(Integer.valueOf(attributes.getValue("status"))) {
+				case 1:
+					curBookmark.put("description", "S / "+attributes.getValue("description"));
+					break;
+				case 2:
+					curBookmark.put("description", "P / "+attributes.getValue("description"));
+					break;
+				default:
+					curBookmark.put("description", attributes.getValue("description"));
+					break;
+                        }
 			curBookmark.put("hash", attributes.getValue("hash"));
 			curBookmark.put("tag", attributes.getValue("tag"));
 			curBookmark.put("time", attributes.getValue("time"));
 			curBookmark.put("status", attributes.getValue("status"));
 			this.bookmarks.add(curBookmark);
+
 		}
 	}
 }
